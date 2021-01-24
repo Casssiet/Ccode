348220cb9b32ebe515957da0c971e2b57192a9a3
Casssiet <2312603961@qq.com>
2021-01-24 17:41:52 +0800

    minus

--- 20210124/20210124/test.c
+++ 20210124/20210124/test.c
@@ -6,8 +6,11 @@ int main()
 	int num2;
 	int num3;
 	int sum;
+	int minus;
 	scanf("%d,%d,%d", &num1, &num2, &num3);
 	sum = num1 + num2 + num3;
+	minus = num1 - num2;
 	printf("sum=%d\n", sum);
+	printf("minus=%d\n", minus);
 	return 0;
 }
