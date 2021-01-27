998a5be123755b5493a83db4d2acea97b2748618
Casssiet <2312603961@qq.com>
2021-01-27 21:45:13 +0800

    zfc

---
+++ 20210127/20210127/test.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+//#define MAX 10
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET,
+//};
+//int main()
+//{
+	/*enum Sex sex = MALE;*/
+	/*printf("%d\n", MALE);
+	printf("%d\n", FEMALE);
+	printf("%d\n", SECRET);*/
+	//int arr[MAX] = { 0 };
+	//printf("%d\n", MAX);
+	/*return 0;*/
+//}
+int main()
+//{
+//	char arr1[] = "abc";
+//	/*char arr2[] = { 'a','b','c' };*/
+//	char arr2[] = { 'a','b','c',0 };
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//{
+//	printf("c:\\test\\32\\test.c");
+//	return 0;
+//}
+{
+	printf("%d\n",strlen("c:\test\32\test.c"));
+	return 0;
+}
