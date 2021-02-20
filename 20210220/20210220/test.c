ad12d8b17c5812ef5d16fb99eb44a3ad51a8d5b5
Casssiet <2312603961@qq.com>
2021-02-20 21:08:06 +0800

    while&for

--- 20210220/20210220/test.c
+++ 20210220/20210220/test.c
@@ -2,17 +2,17 @@
 ////输出1-100之间的奇数
 #define _CRT_SECURE_NO_WARNINGS
 
-#include<stdio.h>
-int main()
-{
-	int a;
-	scanf("%d", &a);
-	if (a % 2 == 1)
-	{
-		printf("%d是奇数\n", a);
-	}
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a % 2 == 1)
+//	{
+//		printf("%d是奇数\n", a);
+//	}
+//	return 0;
+//}
 //#include<stdio.h>
 //int main()
 //{
@@ -32,3 +32,29 @@ int main()
 //	}
 //	return 0;
 //}
+
+#include<stdio.h>
+int main()
+{
+	int ret=0;
+	int ch = 0;
+	char password[20] = { 0 };
+	printf("请输入密码：");
+	scanf("%s", &password);
+	while((ret=getchar())!='\n')//getchar:去\n !=优先级比=高，所以用括号
+	{
+		;
+	}
+	printf("请确定(Y\N)：");
+	ch = getchar();
+	if(ch=='Y')
+	{
+		printf("确定输出\n");
+	}
+	else
+	{
+		printf("放弃输出\n");
+	}
+
+	return 0;
+}
